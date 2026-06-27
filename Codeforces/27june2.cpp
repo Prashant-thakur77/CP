730fb176d83164305fdd395ba977c8fd51956bee
Prashant Thakur <prashant101007@gmail.com>
2026-06-27 21:38:43 +0530

    done

---
+++ Codeforces/27june2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ Codeforces/tempCodeRunnerFile.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    if(n<=k){
+        cout<<n<<endl;
+    }
+    ll no=n/k;
+    ll cursum=0;
+    int no2=1;
+    int l=0;
+    while(no2<=no){
+        
+        if(pow(2,l+1)-1>no){
+            break;
+
+
+        };
+        l++;
+        no2=pow(2,l)-1;
+        
+
+    }
+    ll popcount=0;
+    for(int i=0;i<k;i++){
+        popcount+=l;
+        cursum+=no-no2;
+        if(cursum+no>=pow(2,l+1)-1){
+            popcount++;
+            cursum=cursum+no-(pow(2,l+1)-1);
+        }
+
+
+    }
+    cout<<popcount<<endl;
+    
+
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
