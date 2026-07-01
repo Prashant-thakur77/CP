8b223cf808ebb161e1e1c2b06365cc88ff10e179
Prashant Thakur <prashant101007@gmail.com>
2026-07-01 23:34:14 +0530

    done

---
+++ Codeforces/1july.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int n;
+    cin>>n;
+    int x = 10000, y = 0;
+        for (int i = 0; i < n; ++i) {
+            int z;
+            cin >> z;
+            x = min(x, z), y += x;
+        }
+        cout << y << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
