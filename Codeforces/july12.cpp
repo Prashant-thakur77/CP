694b86aebc5f966358729f8862d4eb5a439921ce
Prashant Thakur <prashant101007@gmail.com>
2026-07-12 23:03:45 +0530

    done

---
+++ Codeforces/july12.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int n;
+    cin>>n;
+    long long ans=0;
+    for(int b=1;b<=n;b++){
+        ans+= 1ll*(n/b)*(n/b);
+    }
+    cout<<ans<<'\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
