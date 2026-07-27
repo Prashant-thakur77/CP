97d0363608fc014686e7d3b8440c20f4b718df62
Prashant Thakur <prashant101007@gmail.com>
2026-07-27 10:09:34 +0530

    done

---
+++ Codeforces/1.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    string s;
+    getline(cin,s);
+    string word;
+    cin>>word;
+    int cnt=0;
+    int ind=-1;
+    for(int i=0;i<s.size();i++){
+       
+        if(s.find(word,i)!=string::npos){
+            int no=s.find(word,i);
+            if(ind<no){
+                ind=no;
+                cnt++;
+            }
+        }
+        else break;
+
+    }
+    cout<<cnt<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
