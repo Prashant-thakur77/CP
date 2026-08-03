77397a3184fdf68bb3e7b848405829cb869afaf6
Prashant Thakur <prashant101007@gmail.com>
2026-08-03 23:57:33 +0530

    done

---
+++ Codeforces/3aug.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+  
+        long long n,a,b;
+        cin>>n>>a>>b;
+
+        long long c=n/3;
+        long long d=n%3;
+
+        cout<<min({(c+1)*b,c*b+d*a,n*a})<<'\n';
+    
+
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
