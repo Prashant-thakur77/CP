b46e68f14df9d8f003c68c744b1ab1d454775b75
Prashant Thakur <prashant101007@gmail.com>
2026-08-05 22:54:33 +0530

    done

---
+++ Codeforces/5.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n),b(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+    }
+
+    ll sum=0;
+    for(int i=0;i<n;i++)sum+=max(a[i],b[i]);
+    ll ans=0;
+    for(int i=0;i<n;i++){
+         ans=max(ans,sum+min(a[i],b[i]));
+
+    }
+    cout<<ans<<endl;   
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
