8fcd6c6987d65d4cdcc2f38104805b56d04ad857
Prashant Thakur <prashant101007@gmail.com>
2026-08-17 10:37:34 +0530

    done

---
+++ Codeforces/input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// This is a single-line comment
+int main() {
+    /* This is
+       a multi-line comment */
+    printf("Hello"); // Print greeting
+    return 0;
+}
---
+++ Codeforces/input1.c
@@ -0,0 +1,4 @@
+int main() {
+    int number = 42;
+    return number;
+}
